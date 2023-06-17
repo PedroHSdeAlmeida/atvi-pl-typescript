eda4c75661dcdd69f5bb19a93bfd8812233384b9
PedroHSdeAlmeida <106109294+PedroHSdeAlmeida@users.noreply.github.com>
2023-06-17 04:17:50 -0300

    Cadastro e listagem de produtos e serviços
    Foram realizadas as seguintes alterações:
    
    Alteração em "main.ts", adicionando toda a lógica para chamar o metodo de cadastro e listagem de produto e seviço
    Alteração em "produto.ts", adicionando o atributo preço
    Alteração em "servico.ts", adicionando o atributo preço
    Criação do  "cadastroProduto.ts", classe que contém o metodo para cadastrar os produtos
    Criação do  "cadastroServico.ts", classe que contém o metodo para cadastrar os serviços
    Criação do  "listagemProduto.ts", classe que contém o metodo para listar os produtos
    Criação do  "listagemServico.ts", classe que contém o metodo para listar os serviços

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,6 +3,10 @@ import Empresa from "../modelo/empresa";
 import CadastroCliente from "../negocio/cadastroCliente";
 import ListagemClientes from "../negocio/listagemCliente";
 import AtualizaCliente from "../negocio/atualizaCliente";
+import CadastroProduto from "../negocio/cadastroProduto";
+import CadastroServico from "../negocio/cadastroServico";
+import ListagemProdutos from "../negocio/listagemProduto";
+import ListagemServicos from "../negocio/listagemServico";
 
 console.log(`Bem-vindo ao melhor sistema de gerenciamento de pet shops e clínicas veterinarias`)
 let empresa = new Empresa()
@@ -13,6 +17,10 @@ while (execucao) {
     console.log(`1 - Cadastrar cliente`);
     console.log(`2 - Listar todos os clientes`);
     console.log(`3 - Atualizar/deletar dados do cliente (Nome, nome soocial, telefone ou pets)`);
+    console.log(`4 - cadastrar produto`);
+    console.log(`5 - cadastrar servico`);
+    console.log(`6 - listar produto`);
+    console.log(`7 - listar servico`);
     console.log(`0 - Sair`);
 
     let entrada = new Entrada()
@@ -33,7 +41,21 @@ while (execucao) {
             atualizarCliente.atualiza(pegaCpf)
             break
         case 4:
-        
+            let cadastradoProduto = new CadastroProduto(empresa.getProdutos)
+            cadastradoProduto.cadastrar()
+            break;
+        case 5:
+            let cadastroServico = new CadastroServico(empresa.getServicos)
+            cadastroServico.cadastrar()
+            break;
+        case 6:
+            let listagemProdutos = new ListagemProdutos(empresa.getProdutos)
+            listagemProdutos.listar()
+            break;
+        case 7:
+            let listagemServicos = new ListagemServicos(empresa.getServicos)
+            listagemServicos.listar()
+            break;
         case 0:
             execucao = false
             console.log(`Até mais`)
---
+++ src/negocio/cadastroServico.ts
@@ -0,0 +1,25 @@
+import Entrada from "../io/entrada"
+import Servico from "../modelo/servico"
+import Cadastro from "./cadastro"
+
+export default class CadastroServico extends Cadastro {
+    private servicos: Array<Servico>
+    private entrada: Entrada
+
+    constructor(servicos: Array<Servico>) {
+        super()
+        this.servicos = servicos
+        this.entrada = new Entrada()
+    }
+    
+    public cadastrar(): void {
+        console.log(`\nInício do cadastro do Serviço`);
+        let servico = new Servico()
+        servico.nome = this.entrada.receberTexto('Por favor, digite o nome do serviço: ')
+        servico.preco = this.entrada.receberNumero('Por favor, digite o valor do serviço: ')
+
+        this.servicos.push(servico)
+        console.log(`Serviço cadastrado com sucesso!`)
+    }
+
+}
